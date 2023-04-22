4495cb6412953f20003089fd56f80f75d30a0c68
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-04-22 00:46:03 -0300

    chores: inserting new date parameter in the database

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,13 +3,13 @@ import { ObjectId } from 'mongodb';
 
 export async function postTransaction(req, res) {
 
-    const { title, value } = req.body;
+    const { title, value, date } = req.body;
     const { type } = req.params;
     const { userId } = res.locals.session;
     console.log(type)
 
     try {
-        await db.collection('transactions').insertOne({ title, value, type, userId });
+        await db.collection('transactions').insertOne({ title, value, date, type, userId });
         res.status(201).send('✅ Transaction created SUCESSFULLY!');
     } catch (err) {
         res.status(500).send(`🚫 Unexpected server error!\n\n${err.message}`);
