63f031d2fdf1d214d67b73373187d03df704b411
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-04-22 23:27:05 -0300

    Add username on response for signin

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,7 +23,7 @@ export async function signin(req, res) {
     try {
         const token = uuid()
         await db.collection('sessions').insertOne({ token, userId: user._id })
-        res.send(token);
+        res.send({ name: user.name, token });
     } catch (err) {
         res.status(500).send(`🚫 Unexpected server error!\n\n${err.message}`);
     }
